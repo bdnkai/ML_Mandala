7aeaf6c0a60ede5fa77eff6d581192442e6240d9
bdnkai <bdn.kai@gmail.com>
2023-05-20 20:02:33 -0700

    window capture w/ color filters to single out text

---
+++ main.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+from mandala import process_screen
+
+if __name__ == '__main__':
+
+    screen_img = 'Windows 11 on BDNKAI - Virtual Machine Connection'
+
+    process_screen(screen_img)
+
